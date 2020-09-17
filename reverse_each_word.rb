e4e8623f11e54fd1a7824afd413bc9e91fb2494b
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-17 21:00:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(array)
   
   reverse_array = array.split(" ")
-  reverse_array.map {|word| word.reverse}
-  reverse_array.join(" ")
+  reverse_array.collect {|word| word.upcase}
   
 end
